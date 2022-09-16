13cd1d2e90914fbe6df69b527f94ee634b8fb7b2
jiendra <jitendra@banshkar.com>
2022-09-16 22:37:12 +0530

    UC_3

--- Test/UserRegistrationTest.java
+++ Test/UserRegistrationTest.java
@@ -1,17 +1,22 @@
 import org.junit.jupiter.api.Test;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
-
 class UserRegistrationTest {
     UserRegistration user =new UserRegistration();
     @Test
-    public void givingFirstNameForChecking(){
+    public void givingFirstNameForCheckingValidOrInvalid(){
        String check =user.checkValid("JITENDRA");
        assertEquals("Happy!",check);
     }
     @Test
-    public void givingLastNameForChecking(){
+    public void givingLastNameForCheckingValidOrInvalid(){
         String check =user.checkLastName("BANSHKAR");
         assertEquals("Happy!",check);
     }
+    @Test
+    public void givingEmailForCheckingValidOrInvalid(){
+        String check =user.checkEmail("abc.xyz@bc.com.in");
+        assertEquals("Happy!",check);
+    }
+
 }
--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,6 +1,6 @@
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class UserRegistration {
     public  String checkValid(String firstName){
         Pattern pattern =Pattern.compile("^[A-Z]{3,}+$");
@@ -22,4 +22,21 @@ public class UserRegistration {
             return "Sad";
         }
     }
+    public  String checkEmail(String lastName){
+        Pattern pattern =Pattern.compile("^[a-zA-Z]+[.]+[a-zA-Z]{3}?(@)+[a-zA-Z]+[.]+[a-zA-Z]+[.]+[a-zA-Z]+$");
+        Matcher check =pattern.matcher(lastName);
+        if(check.matches()){
+            return "Happy!";
+        }
+        else {
+            return "Sad";
+        }
+    }
+//    public static void main(String[] args) {
+//        UserRegistration m =new UserRegistration();
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("Enter Email");
+//        String email = sc.nextLine();
+//        System.out.println(m.checkEmail(email));
+//    }
 }
