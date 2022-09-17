2b66c94e563d6f5e1c72fc34a55fb9645dced19a
jiendra <jitendra@banshkar.com>
2022-09-18 03:38:11 +0530

    UC_10

--- Test/UserRegistrationTest.java
+++ Test/UserRegistrationTest.java
@@ -1,51 +1,40 @@
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
+@RunWith(Parameterized.class)
 class UserRegistrationTest {
-    UserRegistration user =new UserRegistration();
-    @Test
-    public void givingFirstNameForCheckingValidOrInvalid(){
-       String check =user.checkValid("JITENDRA");
-       assertEquals("Happy!",check);
-    }
-    @Test
-    public void givingLastNameForCheckingValidOrInvalid(){
-        String check =user.checkLastName("BANSHKAR");
-        assertEquals("Happy!",check);
-    }
-    @Test
-    public void givingEmailForCheckingValidOrInvalid(){
-        String check =user.checkEmail("abc.xyz@bc.com.in");
-        assertEquals("Happy!",check);
-    }
-    @Test
-    public void givingMobileNUmberForCheckingValidOrInvalid(){
-        String check =user.checkMobileWithSpace("91 8035826412");
-        assertEquals("Happy!",check);
-    }
-    @Test
-    public void givingPasswordForCheckingValidOrInvalid(){
-        String check =user.checkPredefinedPassword("jitendrabanshkar");
-        assertEquals("Happy!",check);
-    }
-    @Test
-    public void givingPasswordAtLeastOneUpperForCheckingValidOrInvalid(){
-        String check =user.checkPredefinedPasswordAtLeastOneUpperCase("Jitendra");
-        assertEquals("Happy!",check);
-    }
-    @Test
-    public void givingPasswordAtLeastOneNumberForCheckingValidOrInvalid(){
-        String check =user.checkPredefinedPasswordAtLeastOneNUMber("Jitendra1");
-        assertEquals("Happy!",check);
-    }
-    @Test
-    public void givingPasswordAtLeastOneSpecialCaseForCheckingValidOrInvalid(){
-        String check =user.checkPredefinedPasswordAtLeastOneSpecialCase("Jitendra@1234");
-        assertEquals("Happy!",check);
+      private  String email;
+      private  boolean result;
+      private  UserRegistration user ;
+      public UserRegistrationTest(String email,boolean result){
+          super();
+          this.email=email;
+          this.result=result;
+      }
+      @Before
+      public void initial(){
+          user =new UserRegistration();
+      }
+    @Parameterized.Parameters
+    public  static Collection inputEmail(){
+          return Arrays.asList(new Object[][]{
+                  {"abc.100@yahoo.com",true},
+                  {"abc-100@yahoo.com",true},
+                  {"abc+100@yahoo.com",true},
+                  {"abc100@ab.com.com",false},
+                  {"abc.100@12.com",false},
+                  {"abc.100.df@yahoo.com.com",false},
+                  {"abc.100.@@yahoo.com",false},
+          });
     }
     @Test
     public void givingEmailSamplesProvidedSearatelyForCheckingValidOrInvalid(){
-        String check =user.checkEmailSamplesProvidedSearately("jitendra07@gmail.com");
-        assertEquals("Happy!",check);
+        System.out.println("result"+result);
+    assertEquals(result,user.checkEmailSamplesProvidedSearately(email));
     }
 }
--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -2,94 +2,14 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegistration {
-    public  String checkValid(String firstName){
-        Pattern pattern =Pattern.compile("^[A-Z]{3,}+$");
-        Matcher check =pattern.matcher(firstName);
-        if(check.matches()){
-            return "Happy!";
-        }
-        else {
-            return "Sad";
-        }
-    }
-    public  String checkLastName(String lastName){
-        Pattern pattern =Pattern.compile("^[A-Z]{3,}+$");
-        Matcher check =pattern.matcher(lastName);
-        if(check.matches()){
-            return "Happy!";
-        }
-        else {
-            return "Sad";
-        }
-    }
-    public  String checkEmail(String lastName){
-        Pattern pattern =Pattern.compile("^[a-zA-Z]+[.]+[a-zA-Z]{3}?(@)+[a-zA-Z]+[.]+[a-zA-Z]+[.]+[a-zA-Z]+$");
-        Matcher check =pattern.matcher(lastName);
-        if(check.matches()){
-            return "Happy!";
-        }
-        else {
-            return "Sad";
-        }
-    }
-    public  String checkMobileWithSpace(String mobileNumber){
-        Pattern pattern =Pattern.compile("^?(91)+\\s+[0-9]{10}+$");
-        Matcher check =pattern.matcher( mobileNumber);
-        if(check.matches()){
-            return "Happy!";
-        }
-        else {
-            return "Sad";
-        }
-    }
-    public  String checkPredefinedPassword(String  password){
-        Pattern pattern =Pattern.compile("^[A-Za-z]{8,}+$");
-        Matcher check =pattern.matcher(password);
-        if(check.matches()){
-            return "Happy!";
-        }
-        else {
-            return "Sad";
-        }
-    }
-    public  String checkPredefinedPasswordAtLeastOneUpperCase(String  password){
-        Pattern pattern =Pattern.compile("^[A-Z]{1}+[a-zA-Z]{7,}+$");
-        Matcher check =pattern.matcher(password);
-        if(check.matches()){
-            return "Happy!";
-        }
-        else {
-            return "Sad";
-        }
-    }
-    public  String checkPredefinedPasswordAtLeastOneNUMber(String  password){
-        Pattern pattern =Pattern.compile("^[A-Z]{1}[a-zA-Z]{6,}+[0-9]{1,}+$");
-        Matcher check =pattern.matcher(password);
-        if(check.matches()){
-            return "Happy!";
-        }
-        else {
-            return "Sad";
-        }
-    }
-    public  String checkPredefinedPasswordAtLeastOneSpecialCase(String  email){
-        Pattern pattern =Pattern.compile("^[A-Z]{1}+[a-zA-Z]{5,}[@#%*]+[0-9]{1,}+$");
+    public  boolean checkEmailSamplesProvidedSearately(String  email){
+        Pattern pattern =Pattern.compile("^[a-zA-Z0-9.+-]+[@]+[a-zA-Z]+?(.com)+$");
         Matcher check =pattern.matcher(email);
         if(check.matches()){
-            return "Happy!";
+            return true;
         }
         else {
-            return "Sad";
-        }
-    }
-    public  String checkEmailSamplesProvidedSearately(String  password){
-        Pattern pattern =Pattern.compile("^[a-zA-Z0-9]+?(@gmail.com)+$");
-        Matcher check =pattern.matcher(password);
-        if(check.matches()){
-            return "Happy!";
-        }
-        else {
-            return "Sad";
+            return false;
         }
     }
     public static void main(String[] args) {
